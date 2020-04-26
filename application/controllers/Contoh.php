21954663ed3995a197b55e9d4205f706c1e89011
Choirul-R <rahmadityac@gmail.com>
2020-04-26 13:10:19 +0700

    Still in Beta

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh extends CI_Controller {
+	public function index()
+	{echo "<h1>INI ADALAH CONTOH DEFAULT CONTROLLER </h1>";}
+}
---
+++ application/views/contoh_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Voew</title>
+</head>
+<body>
+    <h1>CONTOH VIEW PADA CODEIGNITER
+    </h1>
+    <hr>
+    <p>Belajar Codeigniter di Framework Indonesia</p>
+</body>
+</html>
